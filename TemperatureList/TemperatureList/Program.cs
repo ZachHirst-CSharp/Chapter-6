c3ea5a73f0f2cdebfc47a0070a8fa0758ef7e1dc
Zach <zach_hirst@live.com>
2018-12-10 13:17:17 -0600

    Push

---
+++ TemperatureList/TemperatureList/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace TemperatureList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tempArray = new int[7];
+            int i;
+
+
+        }
+    }
+}
