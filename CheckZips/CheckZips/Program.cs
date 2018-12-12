1d4e3b137715ab1b63d809b53351b7416e5203eb
Zach <zach_hirst@live.com>
2018-12-12 12:16:37 -0600

    push

--- CheckZips/CheckZips/Program.cs
+++ CheckZips/CheckZips/Program.cs
@@ -11,10 +11,36 @@ namespace CheckZips
     {
         static void Main(string[] args)
         {
-            int[,] zipcodes = { {52801, 52802, 52803, 52804, 52805, 52806, 52807, 52808, 52809, 52810} };
+            int[] zipcodes = {52801, 52802, 52803, 52804, 52805, 52806, 52807, 52808, 52809, 52810};
             int input;
+            int i = 0;
+            bool wrong = true;
 
+            WriteLine("Please enter the desired zip code you would like to ship too. ");
+            input = Convert.ToInt32(ReadLine());
 
+            while(i < 10)
+            {
+                if(input == zipcodes[i])
+                {
+                    wrong = false;
+                    WriteLine("You are in our delivery area. ");
+                }
+                else
+                {
+
+                }
+                ++i;
+            }
+
+            if(wrong == false)
+            {
+
+            }
+            else if(wrong == true)
+            {
+                WriteLine("I'm sorry you're not in our area of delivery. ");
+            }
         }
     }
 }
---
+++ ResortPrices/ResortPrices/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ResortPrices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nights, i = 0, total = 0;
+            int[] nightprice = { 0, 200, 200, 180, 180, 160, 160, 160, 145 };
+
+            WriteLine("Welcome to The CareFree Resort. \n");
+           
+            WriteLine("How many nights would you like to stay? ");
+            nights = Convert.ToInt32(ReadLine());
+
+            if (nights < 8 )
+            {
+                total += nightprice[nights] * nights;
+
+            }
+            else if(nights >= 8)
+            {
+                total += nightprice[8] * nights;
+            }
+            
+
+            WriteLine();
+            WriteLine("Here are our nightly rates.");
+            WriteLine("________________________________");
+            for (i = 0; i < 9; ++i)
+            {
+                if (i < 8)
+                {
+                    WriteLine("The price for " + i + " night(s) is $" + nightprice[i]);
+                }
+                else
+                {
+                    WriteLine("The price for 8 nights or more is $" + nightprice[7]);
+                }
+
+            }
+
+           
+            WriteLine("\n _________________________________");
+            WriteLine("Your total for " + nights + " nights stayed is " + total.ToString("C"));
+        }
+    }
+}
